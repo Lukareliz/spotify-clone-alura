268e102d30b29475af6b29ffe8c37d48d0aeb238
Lucca Nunes <lyulucca@gmail.com>
2024-01-27 18:57:04 -0300

    Aula 4 - Search, JS and json-server

---
+++ script.js
@@ -0,0 +1,35 @@
+const searchInput = document.getElementById('search_input');
+const  resultArtists = document.getElementById('result-artist');
+const resultPlaylist = document.getElementById('result-playlists');
+
+function requestApi(searchTerm){
+    fetch(`http://localhost:3001/artists?name_like=${searchTerm}`)
+        .then((response) => response.json())
+        .then((result) => displayResults(result))
+}
+
+function displayResults(result) {
+    resultPlaylist.classList.add('hidden');
+    const artistName = document.getElementById('artist-name');
+    const artistImage = document.getElementById('artist-img');
+
+    result.forEach(element => {
+        artistName.innerText = element.name;
+        artistImage.src = element.urlImg;
+    });
+
+    resultArtists.classList.remove('hidden');
+
+    console.log(result);
+}
+
+searchInput.addEventListener('input', function(){
+    const searchTerm = searchInput.value.toLowerCase();
+    if(searchTerm === ''){
+        resultPlaylist.classList.add('hidden');
+        resultArtists.classList.remove('hidden');
+        return;
+    }
+
+    requestApi(searchTerm);
+});
